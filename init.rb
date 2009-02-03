e84275a9cc0f045816f74b5b4585b64cd2e7424b
Mike Dvorkin <mike@dvorkin.net>
2009-02-02 20:39:19 -0800

    Extracted the actual plugin code from Fat Free CRM

--- init.rb
+++ init.rb
@@ -1,1 +1,12 @@
-# Include hook code here
+RAILS_DEFAULT_LOGGER.info ">> Adding sample Fat Free CRM plugin..."
+
+FatFreeCRM::Plugin. << :sample_plugin do # Same as FatFreeCRM::Plugin.add(:sample_plugin) do
+  name "Sample Fat Free CRM Plugin"
+  author "Michael Dvorkin"
+  version "1.0"
+  description "Sample 'do-nothing' plugin to test and demonstrate the concepts."
+end
+
+RAILS_DEFAULT_LOGGER.info ">> Fat Free CRM Plugins:\n" + FatFreeCRM::Plugin.list.inspect
+
+require "crm_sample_plugin.rb"
--- lib/crm_sample_plugin.rb
+++ lib/crm_sample_plugin.rb
@@ -1,1 +1,33 @@
-# CrmSamplePlugin
+# Let the magic begin! ;-)
+#------------------------------------------------------------------------------
+class HomeCallback < FatFreeCRM::Callback::Base
+
+  # home/index view hook.
+  #----------------------------------------------------------------------------
+  def home_view(view, context = {})
+    view.logger.info "view: " + view.class.to_s
+
+    # The following gets rendered on the home page as HTML comment -- check
+    # out View Source in your browser to see it!
+    "home_view: hook parameters: " + context.inspect
+  end
+
+  # home/index controller hook.
+  #----------------------------------------------------------------------------
+  def home_controller(controller, context = {})
+    controller.logger.info "controller: " + controller.controller_name
+    controller.logger.info "action: " + controller.action_name
+    controller.logger.info "hello: " + controller.instance_variable_get("@hello")
+    controller.logger.info "home_controller: hook parameters: " + context.inspect
+  end
+
+  # home controller before_filter hook.
+  #----------------------------------------------------------------------------
+  def home_before_filter(controller, context = {})
+    controller.logger.info "controller: " + controller.controller_name
+    controller.logger.info "action: " + controller.action_name
+    controller.logger.info "params: " + controller.params.inspect
+    controller.logger.info "home_before_filter: hook parameters: " + context.inspect
+  end
+
+end
